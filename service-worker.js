31cd9c8db1f93b09c4c1bc494b3e2aaf151fe182
Baezdeayolas <abelito810@gmail.com>
2026-09-16 20:51:47 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'pedidos-koeju-v26';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './data.js',
+  './clientes.js',
+  './manifest.json',
+  './Icons/icon-192.png',
+  './Icons/icon-512.png',
+  './Icons/logo-banner.jpg'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).catch(() => caches.match('./index.html'))
+      );
+    })
+  );
+});
